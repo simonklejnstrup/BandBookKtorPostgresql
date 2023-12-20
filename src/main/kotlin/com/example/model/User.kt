a044a38ff8bc8f8b8d8d49363bd38ac592dc520d
simon <simon.kp@shape.dk>
2023-12-20 23:13:17 +0100

    optimized imports and changed name of user table in DB

--- src/main/kotlin/com/example/model/User.kt
+++ src/main/kotlin/com/example/model/User.kt
@@ -1,7 +1,10 @@
 package com.example.model
 
 import org.ktorm.entity.Entity
-import org.ktorm.schema.*
+import org.ktorm.schema.Table
+import org.ktorm.schema.long
+import org.ktorm.schema.varchar
+import org.ktorm.schema.timestamp
 import java.time.Instant
 
 interface User: Entity<User> {
@@ -10,13 +13,14 @@ interface User: Entity<User> {
         var firstname: String
         var lastname: String
         var email: String
-//        var created: Instant
+        var created: Instant
     }
-    object Users : Table<User>("people") {
+    object Users : Table<User>("app_user") {
         val id = long("id").primaryKey().bindTo(User::id)
         val firstname = varchar("firstname").bindTo(User::firstname)
         val lastname = varchar("lastname").bindTo(User::lastname)
         val email = varchar("email").bindTo(User::email)
-//        val created = timestamp("created").bindTo(User::created)
+        val created = timestamp("created_at").bindTo(User::created)
+
 
 }
