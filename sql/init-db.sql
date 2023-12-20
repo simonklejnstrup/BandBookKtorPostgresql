431a142ef97490e1c57dc45942ec21dd4e533ef1
simon <simon.kp@shape.dk>
2023-12-20 23:11:51 +0100

    renamed user table

--- sql/init-db.sql
+++ sql/init-db.sql
@@ -4,16 +4,17 @@ CREATE TABLE IF NOT EXISTS book
     name varchar NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS people
+CREATE TABLE IF NOT EXISTS app_user
 (
     id   SERIAL PRIMARY KEY,
     firstname VARCHAR(100) NOT NULL,
     lastname VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO book(name) VALUES ('Book #1');
 INSERT INTO book(name) VALUES ('Book #2');
 
-INSERT INTO people(firstname, lastname, email) VALUES ('firstname #1', 'lastname #1', 'email #1');
-INSERT INTO people(firstname, lastname, email) VALUES ('firstname #2', 'lastname #2', 'email #2');
+INSERT INTO app_user(firstname, lastname, email) VALUES ('firstname #1', 'lastname #1', 'email #1');
+INSERT INTO app_user(firstname, lastname, email) VALUES ('firstname #2', 'lastname #2', 'email #2');
