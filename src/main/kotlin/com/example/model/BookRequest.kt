13ab113a6681e04326adf20c06d1215383cb1f98
simon <simon.kp@shape.dk>
2023-12-22 22:52:06 +0100

    cleanup

--- src/main/kotlin/com/example/model/BookRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BookRequest(
-    val name: String
-)
--- src/main/kotlin/com/example/model/BookResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class BookResponse(
-    val id: Long,
-    val name: String
-)
