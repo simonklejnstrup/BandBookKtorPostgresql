a38e89b791cd9c88e990565c9e6ec078e054c0bd
simon <simon.kp@shape.dk>
2023-12-22 22:51:07 +0100

    add configuration for ForumThread and Comment routes in Application.kt

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,8 +1,9 @@
 package com.example
 
 import com.example.plugins.*
-import com.example.route.configureBookRoutes
+import com.example.route.configureCommentRoutes
 import com.example.route.configureEventRoutes
+import com.example.route.configureForumThreadRoutes
 import com.example.route.configureUserRoutes
 import io.ktor.application.*
 import io.ktor.server.engine.*
@@ -10,9 +11,10 @@ import io.ktor.server.netty.*
 
 fun main() {    
     embeddedServer(Netty, port = 8080, host = "127.0.0.1") {
-        configureBookRoutes()
         configureUserRoutes()
         configureEventRoutes()
+        configureForumThreadRoutes()
+        configureCommentRoutes()
         configureSerialization()
     }.start(wait = true)
 }
