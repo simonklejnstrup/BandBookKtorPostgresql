c091c5cd8c5142b827475f5c96c8e945dabf85e4
simon <simon.kp@shape.dk>
2023-12-22 22:53:05 +0100

    add global serialization

--- src/main/kotlin/com/example/plugins/Serialization.kt
+++ src/main/kotlin/com/example/plugins/Serialization.kt
@@ -1,14 +1,26 @@
 package com.example.plugins
 
+import com.example.util.InstantSerializer
+import com.example.util.LocalDateSerializer
+import com.example.util.LocalTimeSerializer
 import io.ktor.application.*
 import io.ktor.features.*
-import io.ktor.request.*
-import io.ktor.response.*
-import io.ktor.routing.*
 import io.ktor.serialization.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalTime
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json {
+            serializersModule = SerializersModule {
+                contextual(Instant::class, InstantSerializer)
+                contextual(LocalDate::class, LocalDateSerializer)
+                contextual(LocalTime::class, LocalTimeSerializer)
+            }
+        }
+        )
     }
 }
